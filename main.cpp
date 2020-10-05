d556415e775e37a1168a3cacdf63c42e8c03620c
Charlie Welsh <tetraoxygen@icloud.com>
2020-10-04 20:53:20 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DummyProject
+//
+//  Created by Charlie Welsh on 10/4/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
